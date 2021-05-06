801d84f775c93b3235833ad2905f7b9033f95194
Radmir Gataullin <QTU100@gmail.com>
2021-05-06 20:31:43 +0300

    Add decoding

--- main.py
+++ main.py
@@ -23,21 +23,54 @@ def code(value, obj_size):
         point_hologram[i] = 1 if hologram[i] > 0 else 0
     return point_hologram
 
+def decode(msg, obj_size):
+    dist_to_hologram = obj_size + 0.5
+    wave_len = 1
+    value = 100
+
+    w = wave_len
+    l = dist_to_hologram
+    N = obj_size
+    F = msg
+    FInv = [0] * N
+    Sl = [0] * N
+    SInvl = [0] * N
+    for i in range(0, N):
+        FInv[i] = 1 - F[i]
+        for j in range(0, N):
+            k = j - i
+            L = np.hypot(l, k)
+            Rcos = np.cos((L / w - np.fix(L / w)) / w * 2 * np.pi)
+            R = F[i] * Rcos
+            Sl[j] = Sl[j] + R
+            RInv = FInv[i] * Rcos
+            SInvl[j] = SInvl[j] + RInv
+    return Sl
+
 def add_random_noise(message, obj_size, error_percent):
-    message_with_errors = array(message).view() 
+    message_with_errors = array(message).view()
     errors = np.fix(obj_size * error_percent).astype(np.int)
-    indexes = random.randint(0, obj_size, size=(errors))
+    indexes = random.choice(obj_size, size=(errors), replace=False)
+    print(indexes)
+
     for index in indexes:
         message_with_errors[index] = 1 if message_with_errors[index] == 0 else 1
     return message_with_errors
 
 size = 256
-message = code(100, size)
-message_with_errors = add_random_noise(message, size, 0.2);
 
-fig, axs = plt.subplots(2)
-axs[0].bar(range(0, size), message)
-axs[1].bar(range(0, size), message_with_errors)
+tries = 3
+fig, axs = plt.subplots(tries * 2)
+for i in range(0, tries):
+    errors = 0.5 * i / tries
+    print(errors)
+    message = code(25, size)
+    message_with_errors = add_random_noise(message, size, errors);
+    decoded_message_with_errors = decode(message_with_errors, size)
+    
+    axs[i * 2].bar(range(0, size), message_with_errors)
+    axs[i * 2 + 1].bar(range(0, size), decoded_message_with_errors)
+fig.tight_layout()
 plt.show()
 
 # tensorflowinputs = tf.placeholder(tf.float32, shape=(None, X_train.shape[1]), name='inputs')
