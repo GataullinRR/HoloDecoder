0cb0cd43aae3378cb6c7059d9d8d361e67779df1
Radmir Gataullin <QTU100@gmail.com>
2021-05-09 21:35:31 +0300

    Fix set generation + refac

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def code(value, obj_size):
     obj_size = 256
     dist_to_hologram = obj_size + 0.5
     wave_len = 1
-    value = 100
 
     prj = [0] * obj_size
     prj[value] = 1
@@ -28,7 +27,6 @@ def code(value, obj_size):
 def decode(msg, obj_size):
     dist_to_hologram = obj_size + 0.5
     wave_len = 1
-    value = 100
 
     w = wave_len
     l = dist_to_hologram
@@ -58,22 +56,7 @@ def add_random_noise(message, obj_size, error_percent):
         message_with_errors[index] = 1 if message_with_errors[index] == 0 else 1
     return message_with_errors
 
-def plot(size):
-    tries = 3
-    fig, axs = plt.subplots(tries * 2)
-    for i in range(0, tries):
-        errors = 0.5 * i / tries
-        print(errors)
-        message = code(25, size)
-        message_with_errors = add_random_noise(message, size, errors);
-        decoded_message_with_errors = decode(message_with_errors, size)
-        
-        axs[i * 2].bar(range(0, size), message_with_errors)
-        axs[i * 2 + 1].bar(range(0, size), decoded_message_with_errors)
-    fig.tight_layout()
-    plt.show()
-
-def generate_set(set_size):
+def generate_set(set_size, size, name):
     set = [0] * set_size
     values = np.random.bytes(set_size)
     for i in range(0, set_size):
@@ -85,14 +68,34 @@ def generate_set(set_size):
         decoded_message_with_errors = decode(message_with_errors, size)
 
         result = {}
-        result['value'] = value
-        result['decoded'] = decoded_message_with_errors
+        result['value'] = value / 256
+        result['decoded'] = tf.keras.utils.normalize(decoded_message_with_errors)[0].tolist()
         result['amount_of_errors'] = amount_of_errors
         set[i] = result
 
-    with open('C:/dev/holodecoder_training_set_1.json', 'w+') as f:
+    with open('C:/dev/' + name, 'w+') as f:
         json_str = json.dumps(set, indent=4)
         f.write(json_str)
+
+def load(name):
+    data = []
+    with open('C:/dev/' + name) as f:
+        data = json.load(f)
+
+    xs = []
+    ys = []
+    errors = []
+    for entry in data:
+        xs.append(entry['decoded'])
+        ys.append([entry['value']])
+        errors.append([entry['amount_of_errors']])
+
+    # xs = tf.keras.utils.normalize(xs, axis = 0)
+    # ys = tf.keras.utils.normalize(ys, axis = 0)
+    xs = np.asarray(xs)
+    ys = np.asarray(ys)
+
+    return (xs, ys, errors)
 
 def create_train_model():
     # Reset the graph
@@ -103,21 +106,10 @@ def create_train_model():
     with open('C:/dev/holodecoder_training_set_1.json') as f:
         data = json.load(f)
 
-    xmin = 0
-    xmax = 0
-    for entry in data:
-        curr_min = min(entry['decoded'])
-        curr_max = max(entry['decoded'])
-        xmin = curr_min if curr_min < xmin else xmin
-        xmax = curr_max if curr_min > xmax else xmax
-    
     xs = []
     ys = []
     for entry in data:
-        #fixed = list(map(lambda x: (x + abs(xmin)) / (abs(xmin) + abs(xmax)), entry['decoded']))
-        #xs.append(fixed)
         xs.append(entry['decoded'])
-        # ys.append([entry['value'] / 256.0])
         ys.append([entry['value']])
 
     xs = tf.keras.utils.normalize(xs)
@@ -137,4 +129,14 @@ def create_train_model():
     model.compile(optimizer='adam', loss='mse', metrics=["mae"])
     model.fit(xs_train, ys_train, epochs=1000, validation_data=(xs_test, ys_test))
 
-create_train_model()
+#create_train_model()
+
+generate_set(10, 256, "set_2.json")
+xs, ys, errors = load('set_2.json')
+cnt = 5
+fig, axs = plt.subplots(cnt)
+for i in range(cnt):
+    print("Err: " + str(errors[i][0]) + ' - y: ' + str(ys[i][0]))
+    axs[i].bar(range(0, xs[i].size), xs[i])
+fig.tight_layout()
+plt.show()
